13d66e066d4a9948377a32531d775f6eb2c5c2eb
leeyong0919 <65996325+leeyong0919@users.noreply.github.com>
2020-06-02 16:00:52 +0900

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-  fields = ['pub_date','question_text']
+  fieldsets = [('Question Statement',{fields':['question_text']}),
+            ('Date Information',{'fields':['pub_date'], 'classes':['collapse']})]
 
 admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
