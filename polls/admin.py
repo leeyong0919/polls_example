43b6b070c5abad4fc7aaff1301069a2f199d1c49
leeyong0919 <65996325+leeyong0919@users.noreply.github.com>
2020-06-02 16:05:59 +0900

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+class ChoiceInline(admin, TabularInline):
+  model = Choice
+  extra = 2
+
 class QuestionAdmin(admin.ModelAdmin):
-  fieldsets = [('Question Statement',{fields':['question_text']}),
+  fieldsets = [('Question Statement',{'fields':['question_text']}),
             ('Date Information',{'fields':['pub_date'], 'classes':['collapse']})]
+
+  inlines = [ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
