fff072966b269db38814c4c49554cd86bcc2d081
leeyong0919 <65996325+leeyong0919@users.noreply.github.com>
2020-06-02 16:22:32 +0900

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
